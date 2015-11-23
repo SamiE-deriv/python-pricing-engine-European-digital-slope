181b3337a5264bba208b06ad73f0a057aca61c02
Jun Bon Kok <junbon@binary.com>
2015-11-23 09:34:53 +0000

    5% commission for middle eastern submarket

--- lib/Pricing/Engine/EuropeanDigitalSlope.pm
+++ lib/Pricing/Engine/EuropeanDigitalSlope.pm
@@ -447,6 +447,11 @@ sub commission_markup {
     my $self = shift;
 
     return 0    if $self->error;
+
+    # 5% commission for middle eastern submarket
+    return 0.05 if ($self->_underlying_config->{submarket} eq 'middle_eastern');
+
+    # 3% commission for forward starting contracts
     return 0.03 if $self->_is_forward_starting;
 
     state $comm_file = LoadFile(File::ShareDir::dist_file('Pricing-Engine-EuropeanDigitalSlope', 'commission.yml'));
