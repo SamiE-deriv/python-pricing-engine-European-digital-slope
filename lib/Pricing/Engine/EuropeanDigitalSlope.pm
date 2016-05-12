4ad7e5f1301eb0869831044a3ae5c2623ad062b0
Jun <junbon@binary.com>
2016-05-12 09:57:24 +0000

    increase version to 1.14

--- lib/Pricing/Engine/EuropeanDigitalSlope.pm
+++ lib/Pricing/Engine/EuropeanDigitalSlope.pm
@@ -24,11 +24,11 @@ Pricing::Engine::EuropeanDigitalSlope - A pricing model for european digital con
 
 =head1 VERSION
 
-Version 1.13
+Version 1.14
 
 =cut
 
-our $VERSION = '1.13';
+our $VERSION = '1.14';
 
 
 =head1 SYNOPSIS
