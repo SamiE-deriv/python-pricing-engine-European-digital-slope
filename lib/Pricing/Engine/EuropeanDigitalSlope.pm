9ce699dbb0fe2e5f8b6ef735b468b2166e0bf59f
Yng Shan <yngshan@binary.com>
2018-05-28 07:04:01 +0000

    fix the timeinyears required for equal tie markup

--- lib/Pricing/Engine/EuropeanDigitalSlope.pm
+++ lib/Pricing/Engine/EuropeanDigitalSlope.pm
@@ -341,7 +341,7 @@ sub _risk_markup {
         $risk_markup /= 2;
 
         if ($self->apply_equal_tie_markup) {
-            my $equal_tie_markup = Pricing::Engine::Markup::EqualTie->new(underlying_symbol => $self->underlying_symbol)->markup->amount;
+            my $equal_tie_markup = Pricing::Engine::Markup::EqualTie->new(underlying_symbol => $self->underlying_symbol, timeinyears => $self->$self->_timeinyears)->markup->amount;
             $risk_markup += $equal_tie_markup;
             $self->debug_info->{risk_markup}{parameters}{equal_tie_markup} = $equal_tie_markup;
         }
